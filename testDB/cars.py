d9eaa0f1347820c237c661c1837bf8b93bb8f7cc
CHRISTIAN SMITH <ancientkiten@gmail.com>
2015-02-28 10:17:53 +1000

    added db cars

---
+++ testDB/cars.py
@@ -0,0 +1,14 @@
+# import sqlite3
+import sqlite3
+
+# Create a new databaase if the database dosen't already exist
+conn = sqlite3.connect("cars")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE inventory(make TEXT, model TEXT, quantity INT)""")
+
+# close the database connection
+conn.close()
