af28b4ce160e77b2b7be703b273e3a018a81600f
CHRISTIAN SMITH <ancientkiten@gmail.com>
2015-03-01 14:35:45 +1000

    Updates

--- testDB/cars.py
+++ testDB/cars.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 # Create a new databaase if the database dosen't already exist
-conn = sqlite3.connect("cars")
+conn = sqlite3.connect("cars.db")
 
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
