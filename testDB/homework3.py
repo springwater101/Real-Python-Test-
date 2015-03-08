570718d2e26c9a94fcd5888a0078ce89bc51875b
CHRISTIAN SMITH <ancientkiten@gmail.com>
2015-03-08 18:39:07 +1000

    update real python

---
+++ testDB/homework3.py
@@ -0,0 +1,33 @@
+# SQLite Functions
+
+import sqlite3
+'''
+with sqlite3.connect("cars.db") as connection:
+	cursor = connection.cursor()
+	# create a dictonary of sql querries
+	sql = {'average':'SELECT avg(population) FROM population',
+		'maximum': 'SELECT max(population) FROM population',
+		'minimum': 'SELECT min(population) FROM population',
+		'sum': 'SELECT sum(population) FROM population',
+		'count': 'SELECT count(city) FROM population'}
+
+	# run each sql query item in the dictionary
+	for keys, values in sql.iteritems():
+		# run sql
+		cursor.execute(values)
+
+		# fetchone() retrieves one record from the  query
+		results = cursor.fetchone()
+		# output theresults to screen
+		print keys + ":", results[0]
+'''
+with sqlite3.connect("cars.db") as connection:
+	cursor = connection.cursor()
+	# create a dictonary of  sql querries
+	cursor.execute('SELECT count(make) FROM orders WHERE make=? and model=?')
+
+	fettch = cursor.fetchall()
+
+	print fettch
+
+	
